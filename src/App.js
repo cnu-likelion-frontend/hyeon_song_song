3f8b2da271840c0116badf246261fb690e1cd676
yeongju <syj7632@naver.com>
2024-04-30 21:40:27 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoListTitle from "./components/TodoListTitle";
 
 function App() {
   return (
---
+++ src/components/TodoListTitle.js
@@ -0,0 +1,6 @@
+function TodoListTitle() {
+    return (
+        <p>T O D O</p>
+        <img src="./images/icon-sun.svg"></img>
+    )
+}
