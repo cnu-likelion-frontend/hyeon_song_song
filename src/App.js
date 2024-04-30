871020568f80bcfb9075d21fd8e4b77d238aeb3e
hyeon002 <hhj48770@naver.com>
2024-04-30 21:56:47 +0900

    Chore: Add page file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import TodoListTitle from "./components/TodoListTitle";
+import Page from "./Page.js";
 
 function App() {
   return (
     <div className="App">
-      
+      <Page />
     </div>
   );
 }
--- src/page.js
+++ src/page.js
@@ -1,8 +1,15 @@
-import './Page.css';
+import TodoListTitle from './components/TodoListTitle';
+import TodoListInput from './components/TodoListInput';
+import TodoListOutput from './components/TodoListOutput';
+import './page.css';
 
 function Page() {
   return (
-    <div></div>
+    <div>
+      <TodoListTitle />
+      <TodoListInput />
+      <TodoListOutput />
+    </div>
   );
 }
 
