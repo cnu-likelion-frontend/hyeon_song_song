ab3cbbe313d071b5706da67d513807221901d4ec
thdtmdrud <thdtmdrud72@gamil.com>
2024-05-02 20:46:21 +0900

    layout setting

--- src/Page.js
+++ src/Page.js
@@ -2,13 +2,17 @@ import TodoListTitle from "./components/TodoListTitle";
 import TodoListInput from "./components/TodoListInput";
 import TodoListOutput from "./components/TodoListOutput";
 import "./page.css";
+import cover from "./images/bg-desktop-light.jpg";
 
 function Page() {
   return (
     <div className="Page">
-      <TodoListTitle />
-      <TodoListInput />
-      <TodoListOutput />
+      <div className="Page_background"></div>
+      <div className="Page_container">
+        <TodoListTitle />
+        <TodoListInput />
+        <TodoListOutput />
+      </div>
     </div>
   );
 }
