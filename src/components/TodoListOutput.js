6549ac8d8462975ed794b5ae5d52fa914b51373e
yeongju <syj7632@naver.com>
2024-05-07 20:00:34 +0900

    todolist output

--- src/components/TodoListOutput.js
+++ src/components/TodoListOutput.js
@@ -12,7 +12,16 @@ function TodoListOutput(){
           <TodoListItem />
           <TodoListItem />
         </div>
-      </div>
+        <div className='LowerContainer'>
+          <p>item left</p>
+          <div className='BtnContainer'>
+          <button className='MiddleBtn'>All</button>
+          <button className='MiddleBtn'>Active</button>
+          <button className='MiddleBtn'>Completed</button>
+          </div>
+          <p>Clear Completed</p>
+        </div>
+    </div>
   )
 }
 
