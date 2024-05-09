ac2860bb05f436f95d0ff0acb4784326dd06d2d0
hyeon002 <hhj48770@naver.com>
2024-05-09 20:30:59 +0900

    Feat: TodoListInput

--- src/Page.js
+++ src/Page.js
@@ -4,14 +4,27 @@ import TodoListOutput from "./components/TodoListOutput";
 import TodoListItem from "./components/TodoListItem";
 import "./page.css";
 import cover from "./images/bg-desktop-light.jpg";
+import { useState, useRef } from 'react';
 
 function Page() {
+  const [ todo, setTodo ] = useState([]);
+  const idRef = useRef(0);
+  const onCreate = (content) => {
+    const newItem = {
+      id: 0,
+      content,
+      isDone: false,
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
   return (
     <div className="Page">
       <div className="Page_background"></div>
       <div className="Page_container">
         <TodoListTitle />
-        <TodoListInput />
+        <TodoListInput onCreate={onCreate}/>
         <TodoListOutput />
       </div>
     </div>
