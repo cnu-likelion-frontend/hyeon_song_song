43f353141c3dd0dd45ef3a037f552ae2a6bf727f
yeongju <syj7632@naver.com>
2024-05-13 15:14:31 +0900

    아래버튼색상

--- src/components/TodoListOutput.js
+++ src/components/TodoListOutput.js
@@ -20,11 +20,28 @@ function TodoListOutput(){
         <div className='LowerContainer'>
           <p>item left</p>
           <div className="BtnContainer">
-            <button className="MiddleBtn" onClick={allBtnClick}>
+            <button
+              className={activeButton === "All" ? "button_Color" : "BasicBtn"}
+              onClick={() => handleButtonClick("All")}
+            >
               All
             </button>
-            <button className="MiddleBtn">Active</button>
-            <button className="MiddleBtn">Completed</button>
+            <button
+              className={
+                activeButton === "Active" ? "button_Color" : "BasicBtn"
+              }
+              onClick={() => handleButtonClick("Active")}
+            >
+              Active
+            </button>
+            <button
+              className={
+                activeButton === "Completed" ? "button_Color" : "BasicBtn"
+              }
+              onClick={() => handleButtonClick("Completed")}
+            >
+              Completed
+            </button>
           </div>
           <button className='ClearBtn'>Clear Completed</button>
         </div>
